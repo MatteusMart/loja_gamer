02a0b3690645178458c8f23ffb8ec808dbef534a
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-05-13 21:18:16 -0300

    Responsivo final adicionado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+
+
+// Inicializa o Carousel
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
   });
+
+  // Função que oculta e exibe menu
+  const exibeMenu = () => {
+    // Muda a visualização dos itens com a classe oculta menu, mudando de hide(oculto) para show(exibir)
+    $('.oculta-navbar').toggle()
+  }
+  // final da função que oculta e exibe o menu 
